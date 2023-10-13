0c1e803ca3ffe549bcfd5d566eab6a72e6de9432
AmisadaiLopez <147724801+AmisadaiLopez@users.noreply.github.com>
2023-10-12 19:39:56 -0600

    Add files via upload

---
+++ src/main/java/Punto.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author amylo
+ */
+public class Punto {
+    int x, y;
+
+    public Punto() {
+    }
+    //se crea constructor 
+
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    //sobrecarga dos metodos que se llaman igual, con parametros diferentes
+    //sobreescritura se utiliza para ya no usar imprimir punto si se va a seguir usando
+    public void imprimirPunto(){
+        System.out.println("x="+x+", y="+y);
+    }
+
+    @Override //anotacion para empezar a programar 
+    public String toString() {
+        //metodo deprecado
+        return "Punto{" + "x=" + x + ", y=" + y + '}';
+    }
+    
+}
